5f02525683aa619ce9cbafe6e673e7049910f342
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2022-12-17 23:48:35 +0200

    знову поправки

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,15 +4,26 @@ const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
+onInitform();
 
 const formFilters = {};
+let feedbackFormInitial = {
+  email: '',
+  message: '',
+};
 
 function onFormInput(evt) {
   formFilters[evt.target.name] = evt.target.value;
+  if (!localStorage.getItem('feedback-form-state')) {
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify(feedbackFormInitial)
+    );
+  }
   localStorage.setItem('feedback-form-state', JSON.stringify(formFilters));
 }
+
 function onFormSubmit(evt) {
-  onInitform();
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   evt.preventDefault();
   evt.currentTarget.reset();
@@ -21,11 +32,9 @@ function onFormSubmit(evt) {
 
 function onInitform() {
   const feedbackForm = JSON.parse(localStorage.getItem('feedback-form-state'));
-  let email = document.querySelector('.feedback-form input');
-  let message = document.querySelector('.feedback-form  textarea');
-  if (!localStorage.getItem('feedback-form-state')) {
-    (email = ''), (message = '');
-  }
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form  textarea');
+
   if (feedbackForm) {
     email.value = feedbackForm.email;
     message.value = feedbackForm.message;
