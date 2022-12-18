b77c7e411192e5cf41bdcb55f94d3febdb5d78cd
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2022-12-18 13:43:00 +0200

    поправки

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,52 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form  textarea');
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 onInitform();
 
-const formFilters = {};
 let feedbackFormInitial = {
   email: '',
   message: '',
 };
 
-function onFormInput(evt) {
-  formFilters[evt.target.name] = evt.target.value;
-  if (!localStorage.getItem('feedback-form-state')) {
-    localStorage.setItem(
-      'feedback-form-state',
-      JSON.stringify(feedbackFormInitial)
-    );
-  }
-  localStorage.setItem('feedback-form-state', JSON.stringify(formFilters));
+if (!localStorage.getItem('feedback-form-state')) {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormInitial)
+  );
 }
 
+function onFormInput(evt) {
+  feedbackFormInitial[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormInitial)
+  );
+}
 function onFormSubmit(evt) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   evt.preventDefault();
+  if (!feedbackFormInitial.email || !feedbackFormInitial.message) {
+  } else {
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  }
   evt.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
+
+  feedbackFormInitial.email = '';
+  feedbackFormInitial.message = '';
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormInitial)
+  );
 }
 
 function onInitform() {
   const feedbackForm = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form  textarea');
 
   if (feedbackForm) {
     email.value = feedbackForm.email;
